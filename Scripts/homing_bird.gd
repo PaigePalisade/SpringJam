7f8bf62c809097092bb3260655ef91b32227c6ac
Paige West <48529882+PaigePalisade@users.noreply.github.com>
2026-04-19 00:45:40 -0400

    enemy gets damaged

--- Scripts/homing_bird.gd
+++ Scripts/homing_bird.gd
@@ -5,10 +5,13 @@ extends Sprite2D
 var timer: float = 12.0
 var spawned = false
 
+@export var health: int = 100
+
 var vel: Vector2
 
 func _process(delta: float) -> void:
 	timer -= delta
+	
 	if timer < 10.0 and timer > 5.0:
 		if not spawned:
 			global_position = Vector2(
@@ -24,7 +27,20 @@ func _process(delta: float) -> void:
 		var target_rotation: float = (get_node("../Bird").global_position - global_position).angle()
 		rotation = rotate_toward(rotation, target_rotation, delta * 2.0)
 		translate(Vector2(1,0).rotated(rotation) * delta * 500.0)
-	if timer < 0.0:
+	if timer < 0.0 or health == 0:
 		timer = 12.0
 		spawned = false
+		health = 100
+		global_position = Vector2(-100,-100)
 		hide()
+		
+	if (get_global_mouse_position() - global_position).x > 0:
+		flip_v = false
+	else:
+		flip_v = true
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	var obj = area.get_parent()
+	if obj is Bullet:
+		health -= 10
+		obj.queue_free()
